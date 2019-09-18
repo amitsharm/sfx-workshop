a57483cc0d63328f6f388bf6fcca809843452a9e
Amit Sharma <4932559+amitsharm@users.noreply.github.com>
2019-09-18 10:58:20 -0700

    Update background.sh

--- sfx-workshop/background.sh
+++ sfx-workshop/background.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/amitsharm/sfx-workshop
+git clone https://github.com/GoogleCloudPlatform/microservices-demo.git
 ln -s /root/sfx-workshop/assets/ /root/assets
 helm init
