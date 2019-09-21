2a7a982db7a0f4d9614d808568f5dc5b41c87a1e
Amit Sharma <4932559+amitsharm@users.noreply.github.com>
2019-09-20 22:21:03 -0700

    Update background.sh

--- sfx-workshop/background.sh
+++ sfx-workshop/background.sh
@@ -2,3 +2,6 @@ git clone https://github.com/amitsharm/sfx-workshop
 git clone https://github.com/GoogleCloudPlatform/microservices-demo.git
 ln -s /root/sfx-workshop/assets/ /root/assets
 helm init
+cd sfx-workshop
+export GOOGLE_APPLICATION_CREDENTIALS=service-account-key.json
+export KUBECONFIG=kubeconfig.yaml
